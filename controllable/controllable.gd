e4f07f68c9ae13fb285d9f9c81ea6abd8e07a2c3
FK <tluckycrab@gmail.com>
2022-12-13 13:28:56 -0600

    basic player structure

---
+++ controllable/controllable.gd
@@ -0,0 +1,34 @@
+extends KinematicBody
+
+var velocity = Vector3.ZERO
+var turn_speed = 0.04
+var move_speed = 20
+var gravity = Vector3.DOWN * 9
+var force = Vector3.ZERO
+
+onready var body = $Armature
+
+func _physics_process(_delta):
+	controls()
+	move()
+
+func move():
+	#movement will always be rotated to your facing, force will not
+	var v = velocity.rotated(Vector3.UP, body.rotation.y) * move_speed
+	var _discard = move_and_slide(v + gravity + force)
+	
+func get_wasd():
+	var wasd = Vector3.ZERO
+	wasd.z = Input.get_action_strength("S") - Input.get_action_strength("W")
+	wasd.x = Input.get_action_strength("A") - Input.get_action_strength("D")
+	return wasd
+	
+func controls():
+	var wasd = get_wasd()
+	velocity = Vector3(0, 0, -wasd.z)
+	key_turn()
+
+func key_turn():
+	var wasd = get_wasd()
+	if wasd.x != 0:
+		body.rotate(Vector3.UP, turn_speed * wasd.x)
